79f4bdd49162af0b50c8d4b6f42d68051d2b29b6
mbllt <ballet.mia.6@gmail.com>
2022-08-30 17:58:38 +0200

    mini update on channel so that it can compile

--- srcs/Channel/Channel.hpp
+++ srcs/Channel/Channel.hpp
@@ -1,19 +1,25 @@
-//ifndef
+#ifndef CHANNEL_HPP
+#define CHANNEL_HPP
 
+#include "Commons.hpp"
+#include "Client.hpp"
 
-//include
 class Channel {
 
+	public :
 		Channel();
 		//canonical form
 		~Channel();
 		// send_priv_msg(); //??
-		// broadcast();
+		void broadcast();
 
 	private :
 		std::string _name;
 		std::string _topic;
 		Client *_operator_client; //or operator_fd?? or username??
-		// std::map<std::string client_name, Client *> _members; //client_name or fd or username
+ 
+		std::map<std::string, Client*> _members; //client_name or fd or username // std::string client_name, Client* client
 
 };
+
+#endif
