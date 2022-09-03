c277a0b53c05985b8158d5e1c8ea4200d50ad95e
llecoq <llecoq@student.42lyon.fr>
2022-09-03 11:00:53 +0200

    ExecutionManager::join() redesign with cleaner switch but still in progress

--- srcs/ExecutionManager/commands/join.cpp
+++ srcs/ExecutionManager/commands/join.cpp
@@ -1,6 +1,7 @@
 #include "ExecutionManager.hpp"
 
 enum e_channel {
+	IGNORE,
 	INVITE_ONLY,
 	CHANNEL_FOUND,
 	USER_IS_IN_CHAN,
@@ -8,34 +9,8 @@ enum e_channel {
 	CHANNEL_NOT_FOUND
 };
 
-typedef std::pair<Channel::iterator, Channel::iterator>	iterator_pair;
-
-static int	find_channel(iterator_pair it_pair, std::string channel_name, Client *client)
-{
-	Channel *channel = it_pair.first->second;
-
-	if (it_pair.first == it_pair.second) { // if channel not found
-		if (channel_name.size() > 50)
-			return BAD_CHANNEL_NAME;
-		return CHANNEL_NOT_FOUND;
-	}
-	else {
-		if (channel->user_is_in_channel(client) == true)
-			return USER_IS_IN_CHAN;
-		if (channel->get_mode() == INVITE_ONLY)
-			return INVITE_ONLY;
-		return CHANNEL_FOUND;
-	}
-}
-
-static void	create_new_channel(Channel::map &channel_book, Client *client, std::string channel_name)
-{
-	Channel	*new_channel = new Channel(channel_name);
-	// add channel to book
-	channel_book.insert(Channel::pair(channel_name, new_channel));
-	client->join_channel(new_channel); // add channel to client's joined_channel and client to channel's members
-	new_channel->set_operator(client);
-}
+static int	find_channel(Channel::map &channel_book, std::string &channel_name, Client *client);
+static void	create_new_channel(Channel::map &channel_book, Client *client, std::string channel_name);
 
 int ExecutionManager::join(Client *client, token_vector tokens) {
 	std::string	cmd("JOIN");
@@ -50,34 +25,58 @@ int ExecutionManager::join(Client *client, token_vector tokens) {
 	token_vector			channels = _split(tokens[1], ",");
 
 	for (size_t  i = 0; i < channels.size(); i++) {
-		// to lower cases
-		for (std::string::size_type j = 0; j < channels[i].length(); ++j)
-			channels[i][j] = std::tolower(channels[i][j]);
-		if (channels[i][0] == '#'){ // channel name starts with # (# alone is a valid name)
-			std::string			channel_name = channels[i];
-			Channel::iterator	chan_it = _channel_book.find(channel_name);
-
-			switch (find_channel(iterator_pair(chan_it, _channel_book.end()), channel_name, client))
-			{
-			case BAD_CHANNEL_NAME:
-				_send_rpl(client, ERR_BADCHANNAME(channel_name), 479);
-				break;
-			case CHANNEL_NOT_FOUND:
-				create_new_channel(_channel_book, client, channel_name);
-				_send_channel_update(channel_name, client, MSG_JOIN(channel_name, client->get_nickname()));				
-				break;
-			case USER_IS_IN_CHAN:
-				// send ERR_USERONCHANNEL and continue //this err is not in rfc join section
-				break;
-			case INVITE_ONLY:
-				break;
-			case CHANNEL_FOUND:
-				client->join_channel(chan_it->second); // add channel to client's joined_channel and client to channel's members
-				_send_channel_update(channel_name, client, MSG_JOIN(channel_name, client->get_nickname()));
-				break;
-			}
-
+		switch (find_channel(_channel_book, channels[i], client))
+		{
+		case BAD_CHANNEL_NAME:
+			_send_rpl(client, ERR_BADCHANNAME(channels[i]), 479);
+			break;
+		case CHANNEL_NOT_FOUND:
+			create_new_channel(_channel_book, client, channels[i]);
+			_send_channel_update(channels[i], client, MSG_JOIN(channels[i], client->get_nickname()));				
+			break;
+		case USER_IS_IN_CHAN:
+			// send ERR_USERONCHANNEL and continue //this err is not in rfc join section
+			break;
+		case INVITE_ONLY:
+			break;
+		case CHANNEL_FOUND:
+			client->join_channel(_channel_book.find(channels[i])->second); // add channel to client's joined_channel and client to channel's members
+			_send_channel_update(channels[i], client, MSG_JOIN(channels[i], client->get_nickname()));
+			break;
 		}
 	}
 	return SUCCESS;
+}
+
+static int	find_channel(Channel::map &channel_book, std::string &channel_name, Client *client)
+{
+	for (std::string::size_type i = 0; i < channel_name.length(); ++i)
+		channel_name[i] = std::tolower(channel_name[i]);
+	
+	if (channel_name[0] == '#') { // channel name starts with # (# alone is a valid name)
+		Channel::iterator	chan_it = channel_book.find(channel_name);
+		Channel	*channel = chan_it->second;
+
+		if (chan_it == channel_book.end()) { // if channel not found
+			if (channel_name.size() > 50)
+				return BAD_CHANNEL_NAME;
+			return CHANNEL_NOT_FOUND;
+		}
+		else {
+			if (channel->user_is_in_channel(client) == true)
+				return USER_IS_IN_CHAN;
+			if (channel->get_mode() == INVITE_ONLY)
+				return INVITE_ONLY;
+			return CHANNEL_FOUND;
+		}
+	}
+	return IGNORE;
+}
+
+static void	create_new_channel(Channel::map &channel_book, Client *client, std::string channel_name)
+{
+	Channel	*new_channel = new Channel(channel_name);
+	channel_book.insert(Channel::pair(channel_name, new_channel)); // add channel to _channel_book
+	client->join_channel(new_channel); // add channel to client's joined_channel and client to channel's members
+	new_channel->set_operator(client);
 }
