fc2720e92e40c468d5d20b06aee6e4f1281706da
Ariane <ariane@iMac-de-Ariane.local>
2022-09-06 22:24:11 +0200

    added numeric replies

--- includes/numeric_replies.hpp
+++ includes/numeric_replies.hpp
@@ -6,7 +6,7 @@
 /*   By: ariane <ariane@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/28 13:34:23 by llecoq            #+#    #+#             */
-/*   Updated: 2022/09/06 16:29:48 by ariane           ###   ########.fr       */
+/*   Updated: 2022/09/06 16:56:30 by ariane           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,9 @@
 #define ERR_CANNOTSENDTOCHAN(channel)			"404 " + channel + " :Cannot send to channel" + CRLF
 #define ERR_NORECIPIENT(command)				"411 :No recipient given " + command + CRLF
 #define ERR_USERONCHANNEL(nickname, channel)	"443 " + nickname + " " + channel + " :is already on channel" + CRLF
-#define ERR_NOTREGISTERED						"451 :You have not registrered" + CRLF
+#define ERR_NOTREGISTERED						"451 :You have not registrered\r\n"
 #define ERR_NEEDMOREPARAMS(command)				"461 " + command + " :Not enough parameters" + CRLF
-#define ERR_ALREADYREGISTRED					"462 :Unauthorized command (already registered)" + CRLF
+#define ERR_ALREADYREGISTRED					"462 :Unauthorized command (already registered)\r\n"
 #define ERR_INVITEONLYCHAN(channel)				"473 " + channel + " :Cannot join channel (+i)" + CRLF
 #define ERR_BADCHANNAME(channel)				"479 " + channel + " :Invalid channel name" + CRLF
 #define ERR_NOSUCHCHANNEL(channel)				"403 " + channel + " :No such channel" + CRLF
@@ -36,6 +36,7 @@
 #define RPL_WELCOME(nickname)					"001 " + nickname + " :Bijour " + nickname + CRLF
 #define RPL_NOTOPIC(nickname, channel) 			"331 " + nickname + " " + channel + " :No topic is set" + CRLF
 #define RPL_TOPIC(nickname, channel, topic) 	"332 " + nickname + " " + channel + " :" + topic + CRLF
+
 #define RPL_NAMREPLY(channel, nickname)			"353 " + nickname + " " + channel + " :" + nickname + CRLF
 #define RPL_ENDOFNAMES(channel, nickname)		"366 " + nickname + " " + channel + " :End of /NAMES list" + CRLF
 
